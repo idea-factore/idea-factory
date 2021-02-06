c4f230f39f6d24684b169c1e4ca50dfebc6e539b
ryan-snyder <36665494+ryan-snyder@users.noreply.github.com>
2021-02-06 13:56:29 -0400

    Update Header.jsx

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -3,10 +3,10 @@ import { PageHeader } from "antd";
 
 export default function Header() {
   return (
-    <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
+    <a href="https://twitter.com/Ideafactoryidea" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Ethereum dev stack focused on fast product iteration"
+        title="Idea Factory"
+        subTitle="Decentralized Funding Pools for Ideas"
         style={{ cursor: "pointer" }}
       />
     </a>
