773b79c20e47d4ba1ff318f757cb97268892c933
DESKTOP-HSLA09I\Ryan <spocksbeard16@gmail.com>
2021-02-07 23:53:37 -0400

    Final submission

--- packages/hardhat/contracts/IDEAFactory.sol
+++ packages/hardhat/contracts/IDEAFactory.sol
@@ -18,8 +18,7 @@ contract IDEAFactory is ERC1155 {
         _uri = uri_;
     }
 
-    function mintIdea(string memory name, string memory description, uint256 initalVote) public returns(uint256) {
-        uint256 id = currentIdea;
+    function mintIdea(string memory name, string memory description) public {
         console.log("Minting new idea with ", name, " and id", currentIdea);
         _mint(msg.sender , currentIdea, 1, "");
         emit mintedIdea(msg.sender, currentIdea, name);
@@ -28,25 +27,20 @@ contract IDEAFactory is ERC1155 {
             name: name,
             description: description,
             owner: msg.sender,
-            votes: initalVote
+            votes: 0
         });
         console.log("Created Idea", idea.name, " which was created by ", idea.owner);
         console.logUint(currentIdea);
         ideas.push(idea);
         _incrementIdea();
         console.logUint(currentIdea);
-        return id;
     }
 
-    //assuming that each vote is worth 1 dollar
-    //stake/(votes+stake)
-    //for example: votes = 10, stake = 5 instead of giving the staker 0.5, we give them 5/15
+
     function stakeIdea(uint256 stake, uint256 id, address sender) public {
         console.log("Staking ", stake, " idea");
         setVotes(stake, id);
-
-        uint256 adjustedStake = stake/(getVotes(id) + stake);
-        _mint(sender, id, adjustedStake, "");
+        _mint(sender, id, stake, "");
     }
     function _incrementIdea() private {
         currentIdea++;
