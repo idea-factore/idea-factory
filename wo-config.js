114aaec03f11a32f83b4e80e15a4b9f21c61af3b
ryan-snyder <spocksbeard16@gmail.com>
2021-05-27 17:32:47 +0000

    Fixed config

--- wo-config.js
+++ wo-config.js
@@ -1,8 +1,6 @@
 const ip = require('ip').address();
 
 module.exports = {
-    verbose: true,
-    resources: [ `http-get://${ip}`, `http-get://${ip}:3000`],
-    timeout: 40000,
+    resources: [`http://${ip}:3000`],
     strictSSL: false
 }
