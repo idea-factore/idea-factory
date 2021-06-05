6c2eb06e0b5d124ed1e3ae145d17955c11985c6e
ryan-snyder <spocksbeard16@gmail.com>
2021-06-05 16:53:56 +0000

    ci(circleci): fixed decrypt script

--- decrypt.sh
+++ decrypt.sh
@@ -9,4 +9,5 @@
           eval "$(ssh-agent -s)"
           # Add the key to the authentication agent
           DISPLAY=":0.0" SSH_ASKPASS="/tmp/askpass" setsid ssh-add /tmp/git_deploy_key </dev/null
-          export GITHUB_TOKEN = $(cat /tmp/git_deploy_key)
+          token = $(cat /tmp/git_deploy_key)
+          export GITHUB_TOKEN = echo "${token}"
